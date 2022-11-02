5aae3daa606891754adba68311ae07242018fc16
Gustavo-Dorigon-Coan <gustavodorigonpessoal@gmail.com>
2022-11-02 15:08:24 -0300

    Ajustes nas senhas do usuário

--- frontend-hospital-management/src/pages/SenhaMobile/senhaMobile.tsx
+++ frontend-hospital-management/src/pages/SenhaMobile/senhaMobile.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
 import AppBarMobile from "../../components/AppBarMobile/appBarMobile";
 import PasswordMobile from "../../components/PasswordMobile/passwordMobile";
 import PasswordMobilePriority from "../../components/PasswordMobilePriority/passwordMobilePriority";
 import PasswordUser from "../../components/PasswordUser/passwordUser";
+
+import './senhaMobile.css';
 
 
 export function SenhaMobile(){
@@ -19,6 +22,15 @@ export function SenhaMobile(){
       <div>
         <PasswordUser/>
       </div>
+      <div className='container-position-back'>
+          <div className='container-align-back'>
+            <Link to='/mobileHome'>
+              <button className='my-button-back'>
+                Voltar
+              </button>
+            </Link>
+          </div>
+        </div>
     </div>
   )
 }
